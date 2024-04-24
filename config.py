d08b820975584dd07027390f8464bc7f9e697dae
nazarrudenok-twink <141823378+nazarrudenok-twink@users.noreply.github.com>
2024-04-24 18:54:35 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import pymysql
 
-timeout = 10
-connection = pymysql.connect(
-  charset="utf8mb4",
-  connect_timeout=timeout,
-  cursorclass=pymysql.cursors.DictCursor,
-  db="defaultdb",
-  host="q-note-nazarrudenok.e.aivencloud.com",
-  password="AVNS_TtF4mkbe35BWIDWBbPz",
-  read_timeout=timeout,
-  port=21199,
-  user="avnadmin",
-  write_timeout=timeout,
+HOST = 'q-note-nazarrudenok.e.aivencloud.com'
+USER = 'avnadmin'
+PASSWORD = 'AVNS_TtF4mkbe35BWIDWBbPz'
+DATABASE = 'defaultdb'
+PORT = 21199
+
+conn = pymysql.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE,
+    port=PORT
 )
-  
-cursor = connection.cursor()
+
+cursor = conn.cursor()
